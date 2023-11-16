39a7a734a5b123425d827c1de9a14b36fdc24c90
ALAIKBITTUQOH <119565931+ALAIKBITTUQOH@users.noreply.github.com>
2023-11-16 09:48:48 +0700

    Coba.py

--- Coba.py
+++ Coba.py
@@ -1,3 +1,16 @@
+1. Persamaan Non Linier
+3+x^3-x=4
+
+2. Persamaan Linier
+2x-4=8
+
+Intruksi :
+1. Carilah Solusi dari 2 soal diatas (metode bebas)
+2. Buatlah Program untuk mencari solusi diatas
+3. tunjukkan nilai x di setiap iterasinya
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 Untuk mencari solusi dari persamaan non-linier dan linier, kita dapat menggunakan metode iteratif seperti metode Newton-Raphson atau metode iterasi sederhana.
 Berikut adalah contoh program Python menggunakan metode iterasi sederhana:
 
